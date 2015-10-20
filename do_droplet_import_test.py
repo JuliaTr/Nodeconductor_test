38a1d051c38f1f4d96d9ba0348300dfe535f8773
Julia <juliatrygubniak@gmail.com>
2015-10-20 15:11:46 +0400

    Fix provider deletion, provider creation, resource import check.

--- do_droplet_import_test.py
+++ do_droplet_import_test.py
@@ -14,7 +14,7 @@ class NodeconductorTest(unittest.TestCase):
     def setUp(self):
         self.driver = get_driver(settings.site_url)
         self.is_project_created = False
-      
+        self.is_provider_created = False
 
     def test_create_delete_project_key_resource(self):
         print '%s is going to be loggedin.' % settings.username
@@ -25,70 +25,87 @@ class NodeconductorTest(unittest.TestCase):
         assert username_idt_field.text == settings.user_full_name, 'Error. Another username.'
         time.sleep(10)
         print '%s was loggedin successfully.' % settings.username
+        
         print 'Organization is going to be chosen.'
         choose_organization(self.driver, settings.nec_organization)
         time.sleep(10)
         print 'Organization was chosen successfully.'
+        
         print 'Project is going to be created.'
         create_project(self.driver, settings.project_name)
         time.sleep(10)
         self.is_project_created = True
         print 'Project was created successfully'
+        
+        self.is_provider_created = True 
         print 'Provider is going to be created.'
         create_provider(self.driver, settings.project_name, settings.provider_name)
         time.sleep(5)
-        list_of_providers_field = self.driver.find_element_by_css_selector('[ng-model="generalSearch"]')
-
+        search_provider = self.driver.find_element_by_class_name('object-list')
+        time.sleep(5)        
         try:
-            list_of_providers_field.find_element_by_link_text(settings.provider_name)
+            self.driver.find_element_by_link_text(settings.provider_name)
         except NoSuchElementException:
-            print 'Created provider is found. This is good.'
-        else:
             print 'I cannot find created provider, test fails.'
-        
+        else:
+            print 'Created provider is found. This is good.'  
         print 'Provider was created successfully.'
         time.sleep(10)
+        
         print 'Resource is going to be imported.'
         import_resource(self.driver, settings.project_name, settings.provider_name, settings.resource_name)
         time.sleep(5)
-        list_of_resource_field = self.driver.find_element_by_css_selector('[ng-model="generalSearch"]')
-
+        search_resource = self.driver.find_element_by_class_name('list-item')
+        time.sleep(5)
         try:
-            list_of_resource_field.find_element_by_link_text(settings.resource_name)
+            self.driver.find_element_by_link_text(settings.resource_name)
         except NoSuchElementException:
-            print 'Imported resource is found. This is good.'
-        else:
             print 'I cannot find imported resource, test fails.'
+        else:
+            print 'Imported resource is found. This is good.'
         print 'Resource was imported successfully.'
         time.sleep(10)
+        
         print 'Resource is going to be unlinked.'
         unlink_resource(self.driver, settings.project_name, settings.resource_name)
         time.sleep(10)
-        list_of_resource_field = self.driver.find_element_by_css_selector('[ng-model="generalSearch"]')
-
+        search_field = self.driver.find_element_by_css_selector('[ng-model="generalSearch"]')
+        search_field.clear()
+        search_field.send_keys(settings.resource_name)
+        time.sleep(5)
         try:
-            list_of_resource_field.find_element_by_link_text(settings.resource_name)
+            self.driver.find_element_by_link_text(settings.resource_name)
         except NoSuchElementException:
             print 'I cannot find unlinked resource. This is good.'
         else:
             print 'Unlinked resource is found, test fails.'
         print 'Resource was unlinked successfully.'
-        print 'Provider is going to be removed.'
-        delete_provider(self.driver, settings.project_name, settings.provider_name)
-        time.sleep(10)
-        list_of_provider_field = self.driver.find_element_by_css_selector('[ng-model="generalSearch"]')
-
-        try:
-            list_of_provider_field.find_element_by_link_text(settings.provider_name)
-        except NoSuchElementException:
-            print 'I cannot find removed provider. This is good.'
-        else:
-            print 'Removed provider is found, test fails.'
-        print 'Provider was removed successfully.'
-
+       
     def tearDown(self):
+        print 'Is provider created: ', self.is_provider_created
+        if self.is_provider_created:
+            try:
+                print 'Provider is going to be removed.'
+                time.sleep(10)
+                self.driver.refresh()       
+                delete_provider(self.driver, settings.project_name, settings.provider_name)
+                time.sleep(10)
+                search_field = self.driver.find_element_by_css_selector('[ng-model="generalSearch"]')
+                search_field.clear()
+                search_field.send_keys(settings.provider_name)
+                time.sleep(5)
+                try:
+                    self.driver.find_element_by_link_text(settings.provider_name)
+                except NoSuchElementException:
+                    print 'I cannot find removed provider. This is good.'
+                else:
+                    print 'Removed provider is found, test fails.'
+            except Exception as e:
+                print 'Provider cannot be removed. Error: %s' % e
+                
         if self.is_project_created:
             try:
+                print 'Project is going to be removed.'
                 delete_project(self.driver, settings.project_name)
                 # Check deletion
                 time.sleep(10)
@@ -101,8 +118,8 @@ class NodeconductorTest(unittest.TestCase):
                 else:
                     print 'Project is found, test fails.'
             except Exception as e:
-                print 'Project cannot be deleted. Error: %s' % e
-        
+                print 'Project cannot be deleted. Error: %s' % e       
+
         # close browser
         self.driver.quit()
 
