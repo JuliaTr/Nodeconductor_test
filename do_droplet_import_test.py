fda37c59762f2b9d9fa16f5af95f92274741ad51
Julia <juliatrygubniak@gmail.com>
2015-10-22 13:14:20 +0400

    Add settings, warning massages in tests

--- do_droplet_import_test.py
+++ do_droplet_import_test.py
@@ -9,40 +9,52 @@ from helpers import (login_nodeconductor, get_driver, create_project, delete_pro
                     import_resource, unlink_resource, delete_provider)
 
 
+class Settings(object):
+    site_url = "http://web-test.nodeconductor.com"
+    username = 'Alice'
+    password = 'Alice'
+    user_full_name = 'Alice Lebowski'
+    nec_organization = 'Ministry of Bells'
+    project_name = 'DO test project'
+    provider_name = 'DigitalOceanTest'
+    resource_name = 'FFW3'
+
+
 class NodeconductorTest(unittest.TestCase):
 
     def setUp(self):
-        self.driver = get_driver(settings.site_url)
-        self.is_project_created = False
-        self.is_provider_created = False
+        self.driver = get_driver(Settings.site_url)
+        self.project_exists = False
+        self.provider_exists = False
+        self.resource_exists = False
 
     def test_create_delete_project_key_resource(self):
-        print '%s is going to be loggedin.' % settings.username
-        login_nodeconductor(self.driver, settings.username, settings.password)
+        print '%s is going to be loggedin.' % Settings.username
+        login_nodeconductor(self.driver, Settings.username, Settings.password)
         time.sleep(10)
         # Identify necessary username on loaded main page
         username_idt_field = self.driver.find_element_by_class_name('user-name')
-        assert username_idt_field.text == settings.user_full_name, 'Error. Another username.'
+        assert username_idt_field.text == Settings.user_full_name, 'Error. Another username.'
         time.sleep(10)
-        print '%s was loggedin successfully.' % settings.username
+        print '%s was loggedin successfully.' % Settings.username
         
         print 'Organization is going to be chosen.'
-        choose_organization(self.driver, settings.nec_organization)
+        choose_organization(self.driver, Settings.nec_organization)
         time.sleep(10)
         print 'Organization was chosen successfully.'
         
         print 'Project is going to be created.'
-        create_project(self.driver, settings.project_name)
+        create_project(self.driver, Settings.project_name)
         time.sleep(10)
-        self.is_project_created = True
+        self.project_exists = True
         print 'Project was created successfully'
         
-        self.is_provider_created = True
         print 'Provider is going to be created.'
-        create_provider(self.driver, settings.project_name, settings.provider_name)
+        create_provider(self.driver, Settings.project_name, Settings.provider_name)
+        self.provider_exists = True
         # time.sleep(10)        
         # try:
-        #     self.driver.find_element_by_link_text(settings.provider_name)
+        #     self.driver.find_element_by_link_text(Settings.provider_name)
         # except NoSuchElementException:
         #     print 'I cannot find created provider, test fails.'
         # else:
@@ -51,10 +63,11 @@ class NodeconductorTest(unittest.TestCase):
         time.sleep(10)
         
         print 'Resource is going to be imported.'
-        import_resource(self.driver, settings.project_name, settings.provider_name, settings.resource_name)
+        import_resource(self.driver, Settings.project_name, Settings.provider_name, Settings.resource_name)
+        self.resource_exists = True
         # time.sleep(10)
         # try:
-        #     self.driver.find_element_by_link_text(settings.resource_name)
+        #     self.driver.find_element_by_link_text(Settings.resource_name)
         # except NoSuchElementException:
         #     print 'I cannot find imported resource, test fails.'
         # else:
@@ -63,58 +76,68 @@ class NodeconductorTest(unittest.TestCase):
         time.sleep(10)
         
         print 'Resource is going to be unlinked.'
-        unlink_resource(self.driver, settings.project_name, settings.resource_name)
+        unlink_resource(self.driver, Settings.project_name, Settings.resource_name)
         time.sleep(10)
         # search_field = self.driver.find_element_by_css_selector('[ng-model="generalSearch"]')
         # search_field.clear()
-        # search_field.send_keys(settings.resource_name)
+        # search_field.send_keys(Settings.resource_name)
         # time.sleep(5)
         # try:
-        #     self.driver.find_element_by_link_text(settings.resource_name)
+        #     self.driver.find_element_by_link_text(Settings.resource_name)
         # except NoSuchElementException:
         #     print 'I cannot find unlinked resource. This is good.'
         # else:
         #     print 'Unlinked resource is found, test fails.'
+        self.resource_exists = False
         print 'Resource was unlinked successfully.'
        
     def tearDown(self):
-        print 'Is provider created: ', self.is_provider_created
-        if self.is_provider_created:
+        print 'Provider exists: ', self.provider_exists
+        if self.provider_exists:
             try:
                 print 'Provider is going to be removed.'
                 time.sleep(10)
                 self.driver.refresh()       
-                delete_provider(self.driver, settings.project_name, settings.provider_name)
+                delete_provider(self.driver, Settings.project_name, Settings.provider_name)
+                self.provider_exists = False
                 time.sleep(10)
                 # search_field = self.driver.find_element_by_css_selector('[ng-model="generalSearch"]')
                 # search_field.clear()
-                # search_field.send_keys(settings.provider_name)
+                # search_field.send_keys(Settings.provider_name)
                 # time.sleep(5)
                 # try:
-                #     self.driver.find_element_by_link_text(settings.provider_name)
+                #     self.driver.find_element_by_link_text(Settings.provider_name)
                 # except NoSuchElementException:
                 #     print 'I cannot find removed provider. This is good.'
                 # else:
                 #     print 'Removed provider is found, test fails.'
             except Exception as e:
                 print 'Provider cannot be removed. Error: %s' % e
-                
-        if self.is_project_created:
+        
+        if self.project_exists:
             try:
                 print 'Project is going to be removed.'
-                delete_project(self.driver, settings.project_name)
+                delete_project(self.driver, Settings.project_name)
+                self.project_exists = False
                 # Check deletion
                 # time.sleep(10)
                 # list_of_projects_field = self.driver.find_element_by_class_name('object-list')
 
                 # try:
-                #     list_of_projects_field.find_element_by_link_text(settings.project_name)
+                #     list_of_projects_field.find_element_by_link_text(Settings.project_name)
                 # except NoSuchElementException:
                 #     print 'I cannot find project. This is good.'
                 # else:
                 #     print 'Project is found, test fails.'
             except Exception as e:
                 print 'Project cannot be deleted. Error: %s' % e       
+        
+        if self.resource_exists:
+            print 'Warning! Test cannot unlink resource %s. It has to be unlinked manually.' % Settings.resource_name
+        if self.provider_exists:
+            print 'Warning! Test cannot delete provider %s. It has to be deleted manually.' % Settings.provider_name
+        if self.project_exists:
+            print 'Warning! Test cannot delete project %s. It has to be delete manually.' % Settings.project_name
 
         # # close browser
         # self.driver.quit()
--- settings.py
+++ settings.py
@@ -3,17 +3,17 @@
 # password = '123'
 # user_full_name = 'Trygubniak'
 
-# site_url = "http://web-test.nodeconductor.com"
-# username = 'Alice'
-# password = 'Alice'
-# user_full_name = 'Alice Lebowski'
-# nec_organization = 'Ministry of Bells'
-# project_name = 'OpenStack test project'
-# key_name = 'Openstack test key'
-# resource_name = 'OpenStack test instance'
-# time_after_resource_creation = 180
-# time_wait_after_resource_stopping = 60
-# time_wait_after_resource_removal = 30
+site_url = "http://web-test.nodeconductor.com"
+username = 'Alice'
+password = 'Alice'
+user_full_name = 'Alice Lebowski'
+nec_organization = 'Ministry of Bells'
+project_name = 'OpenStack test project'
+key_name = 'Openstack test key'
+resource_name = 'OpenStack test instance'
+time_after_resource_creation = 180
+time_wait_after_resource_stopping = 60
+time_wait_after_resource_removal = 30
 
 site_url = "http://web-test.nodeconductor.com"
 username = 'Alice'
